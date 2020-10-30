b8e6eb4a811b9cc1502e7311c8a5d722e2041ec9
Calise <julien.calise@orange.fr>
2020-10-30 15:14:05 +0100

    32 - Methode createCSV

--- src/Model/CSVModel.php
+++ src/Model/CSVModel.php
@@ -4,8 +4,9 @@
 namespace App\Model;
 
 
+use League\Csv\Writer;
 use PDO;
-use League\Csv;
+use SplTempFileObject;
 
 class CSVModel
 {
@@ -20,7 +21,28 @@ class CSVModel
         $this->pdo = $pdo;
     }
 
-    public function createCSV(string $idEpreuve):string{
-        return "TODO::Feuille CSV concernant l'épreuve : ".$idEpreuve;
+    public function createCSV(string $idEpreuve): string
+    {
+        $sql = "select p.ID, p.nom, p.prenom, p.mail 
+                from personne p, epreuve e, personne_epreuve pe
+                where p.ID = pe.personne_ID 
+                and :idEpreuve = pe.epreuve_ID";
+        $query = $this->pdo->prepare($sql);
+        $query->bindValue(':idEpreuve', $idEpreuve);
+        $query->execute();
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        $csv= Writer::createFromFileObject(new SplTempFileObject());
+
+        $csv->insertOne(['ID', 'Nom', 'Prenom','Mail','Passage_1','Passage_2']);
+
+        $csv->insertAll($result);
+
+        return $csv->output('Epreuve-'.$idEpreuve.'.csv');
+    }
+
+    public function putCSVinBDD(string $idEpreuve)
+    {
+
     }
 }
